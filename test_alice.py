7b59c139d250a930f120dc70ffbf2a62a4163853
JiDe Zhang <zhangjide@deepin.org>
2026-01-16 17:32:03 +0800

    Remove temporary test files

--- test_alice.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2020 Alice Corporation
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-def test_alice():
-    """Test function from Alice Corp."""
-    pass
--- test_copyright.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2020 UnionTech Software Technology Co., Ltd.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-def test_copyright_format():
-    """Test file using Copyright (C) format."""
-    pass
