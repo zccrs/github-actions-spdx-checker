9396e1a57ff5551d86f52f0b665acb6842f94744
JiDe Zhang <zhangjide@deepin.org>
2026-01-16 17:10:57 +0800

    Test copyright format file

---
+++ test_copyright.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2020 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+def test_copyright_format():
+    """Test file using Copyright (C) format."""
+    pass
